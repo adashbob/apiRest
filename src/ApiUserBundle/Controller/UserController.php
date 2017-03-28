0e1dfa62507282c00168b417a8d1bc9ee320dba4
adashbob <bobodiallobe@gmail.com>
2017-03-28 10:00:28 +0000

    managging user

--- src/ApiUserBundle/Controller/UserController.php
+++ src/ApiUserBundle/Controller/UserController.php
@@ -42,8 +42,7 @@ class UserController extends Controller
    }
 
    /**
-    * Return user by id
-    *
+    * @ApiDoc(description="Get user by id")
     * @Rest\View(serializerGroups={"user"})
     * @Rest\Get("/users/{id}")
     */
@@ -59,8 +58,7 @@ class UserController extends Controller
    }
 
    /**
-    * Remove User
-    *
+    * @ApiDoc(description="Remove user")    *
     * @Rest\View(StatusCode=Response::HTTP_NO_CONTENT, serializerGroups={"user"})
     * @Rest\Delete("/users/{id}")
     */
@@ -76,26 +74,27 @@ class UserController extends Controller
       $auth_codes =  $this->getDoctrine()->getRepository('ApiUserBundle:AuthCode')->findAll();
       $refresh_tokens =  $this->getDoctrine()->getRepository('ApiUserBundle:RefreshToken')->findAll();
 
-      foreach ($access_tokens as $access_token) {
+      foreach ($access_tokens as $access_token)
+      {
          if($access_token->getUser() == $user)
             $em->remove($access_token);
       }
-      foreach ($auth_codes as $auth_code) {
+      foreach ($auth_codes as $auth_code)
+      {
          if($auth_code->getUser() == $user)
             $em->remove($auth_code);
       }
-      foreach ($refresh_tokens as $refresh_token) {
+      foreach ($refresh_tokens as $refresh_token)
+      {
          if($refresh_token->getUser() == $user)
             $em->remove($refresh_token);
       }
       $em->remove($user);
-
       $em->flush();
    }
 
    /**
-    * Add un user
-    *
+    * @ApiDoc(description="Add a user")
     * @param Request $request
     * @return User|\FOS\UserBundle\Model\UserInterface|mixed|\Symfony\Component\Form\Form
     *
@@ -109,19 +108,17 @@ class UserController extends Controller
       $form = $this->createForm(UserType::class, $user);
       $form->submit($request->request->all());
 
-      if($form->isValid()){
+      if($form->isValid())
+      {
          $this->get('fos_user.user_manager')->updateUser($user);
          return $user;
       }
       else
-      {
          return $form;
-      }
    }
 
    /**
-    * Update Uer
-    *
+    * @ApiDoc(description="update a user")
     * @param Request $request
     * @return \Symfony\Component\Form\Form
     *
@@ -147,6 +144,18 @@ class UserController extends Controller
          return $user;
       }
       return $form;
+   }
+
+   /**
+    * @ApiDoc(description="Change user's password")
+    * @param Request $request
+    * @Rest\View(serializerGroups={"user"})
+    * @Rest\Patch("/users/chpass/{id})
+    */
+   public function changePasswordAction(Request $request)
+   {
+      $user = $this->getDoctrine()->getRepository('ApiUserBundle:User')->find($request->get('id'));
+
    }
 
 }
